23cf190531d8617fb236c5f79bc24f730ab54f54
Dizzy B High <dizzybhigh@gmail.com>
2026-09-07 19:07:57 +0200

    Add per-extension RtsUI DLL requirements action

---
+++ streamerbot/SetExtensionDllRequirements.cs
@@ -0,0 +1,11 @@
+// Streamer.bot C# action: define the RtsUI.dll requirements for the next action.
+// The generic RTS - UI DLL Check action reads these arguments.
+public class CPHInline
+{
+    public bool Execute()
+    {
+        CPH.SetArgument("rts.extensionName", "RTS Action Replay");
+        CPH.SetArgument("rts.minimumRtsUiVersion", "0.1.0");
+        return true;
+    }
+}
