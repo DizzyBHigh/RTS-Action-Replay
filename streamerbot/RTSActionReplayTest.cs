25fbcd3c51a957e74575d5c767896f86250fae46
Dizzy B High <dizzybhigh@gmail.com>
2026-09-07 17:29:00 +0200

    Fix Action Replay test broadcast method

--- streamerbot/RTSActionReplayTest.cs
+++ streamerbot/RTSActionReplayTest.cs
@@ -1,17 +1,12 @@
-using System;
-
 public class CPHInline
 {
     public bool Execute()
     {
         var replayUrl = "http://127.0.0.1:7474/replays/Area18%20Valet%20Service.mp4";
+        var json = "{\"type\":\"replay\",\"command\":\"load\",\"url\":\"" + replayUrl + "\"}";
+
         CPH.LogInfo("RTS Action Replay test: loading replay");
-        CPH.WebsocketBroadcast("RTS Action Replay", new
-        {
-            type = "replay",
-            command = "load",
-            url = replayUrl
-        });
+        CPH.WebsocketBroadcastJson(json);
         return true;
     }
 }
