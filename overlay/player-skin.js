b05d433a6cb4d497cdf75fc3c2a4f77bb1d02d3c
Dizzy B High <dizzybhigh@gmail.com>
2026-09-08 17:04:59 +0200

    Anchor slow motion to play speed indicator

--- overlay/player-skin.js
+++ overlay/player-skin.js
@@ -18,11 +18,12 @@ RTSReplaySkin.applyElementPosition = (element, data, defaults) => {
   element.style.left = `${50 + x}%`; element.style.top = `${50 + y}%`; element.style.transform = `translate(-50%, -50%) scale(${scale / 100})`;
 };
 RTSReplaySkin.configureElements = command => {
-  const elements = RTSReplaySkin.readElements(command);
+  const elements = RTSReplaySkin.readElements(command); const speed = elements['Play Speed Indicator'] || RTSReplaySkin.elementDefaults['Play Speed Indicator'];
   RTSReplaySkin.applyElementPosition(RTSReplaySkin.brand, elements.Brand, RTSReplaySkin.elementDefaults.Brand);
   RTSReplaySkin.applyElementPosition(RTSReplaySkin.title, elements.Title, RTSReplaySkin.elementDefaults.Title);
-  RTSReplaySkin.applyElementPosition(RTSReplaySkin.speedLabel, elements['Play Speed Indicator'], RTSReplaySkin.elementDefaults['Play Speed Indicator']);
-  RTSReplaySkin.applyElementPosition(RTSReplaySkin.slowMotion, elements['Play Speed Indicator'], RTSReplaySkin.elementDefaults['Play Speed Indicator']);
+  RTSReplaySkin.applyElementPosition(RTSReplaySkin.speedLabel, speed, RTSReplaySkin.elementDefaults['Play Speed Indicator']);
+  const scale = Math.max(0, Number(speed.scale) || 0); const x = Math.max(-100, Math.min(100, Number(speed.x) || 0)); const y = Math.max(-100, Math.min(100, Number(speed.y) || 0));
+  RTSReplaySkin.slowMotion.style.left = `${50 + x}%`; RTSReplaySkin.slowMotion.style.top = `calc(${50 + y}% + ${1.2 * scale / 100}em)`; RTSReplaySkin.slowMotion.style.transform = `translate(-50%, 0) scale(${scale / 100})`;
 };
 RTSReplaySkin.configureFrame = command => {
   const frame = RTSReplaySkin.frame;
