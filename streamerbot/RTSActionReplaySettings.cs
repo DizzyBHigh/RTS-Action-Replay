19be759376a2e2560e5959d6f3900245b1da63d2
Dizzy B High <dizzybhigh@gmail.com>
2026-09-08 13:56:35 +0200

    Use configured start and end positions

--- streamerbot/RTSActionReplaySettings.cs
+++ streamerbot/RTSActionReplaySettings.cs
@@ -36,13 +36,12 @@ public class CPHInline
         ui.AddDropdown("Border Style", "Visual style of the player border.", "Appearance", "rts.actionreplay.borderStyle", new[] { "None", "Solid", "Dashed", "Double" }, "Solid");
 
         ui.AddTitle("Player Positions", "Positions");
-        ui.AddTextbox("Default Position", "Name of the saved position used when a replay loads.", "Positions", "rts.actionreplay.defaultPosition", "Full Screen", false);
+        ui.AddPositionSelector("Default Start Position", "Position used when the player starts showing.", "Positions", "rts.actionreplay.defaultStartPosition", "rts.actionreplay.positions", "Full Screen");
+        ui.AddPositionSelector("Default End Position", "Position used when the player has finished showing.", "Positions", "rts.actionreplay.defaultEndPosition", "rts.actionreplay.positions", "Full Screen");
         ui.AddPositionEditor("Saved Positions", "Create and edit reusable player positions. Full Screen is built in and cannot be deleted.", "Positions", "rts.actionreplay.positions", "{\"Full Screen\":{\"name\":\"Full Screen\",\"scale\":100,\"x\":0,\"y\":0,\"rotateX\":0,\"rotateY\":0,\"rotateZ\":0}}");
 
         ui.AddTitle("Player Animation", "Animation");
-        ui.AddDropdown("Animation In", "Entrance animation when the player loads.", "Animation", "rts.actionreplay.animationIn", new[] { "None", "Zoom In", "Slide From Left", "Slide From Right", "Slide From Top", "Slide From Bottom" }, "None");
-        ui.AddDropdown("Animation Out", "Exit animation when Streamer.bot hides the player.", "Animation", "rts.actionreplay.animationOut", new[] { "None", "Zoom Out", "Slide To Left", "Slide To Right", "Slide To Top", "Slide To Bottom" }, "None");
-        ui.AddDecimalTextbox("Animation Duration", "Animation duration in seconds.", "Animation", "rts.actionreplay.animationDuration", 0.5, 0.1, 5.0, 0.1);
+        ui.AddDecimalTextbox("Animation Duration", "Duration used when moving between the Start and End positions.", "Animation", "rts.actionreplay.animationDuration", 0.5, 0.1, 5.0, 0.1);
         ui.AddDropdown("Animation Easing", "CSS easing used for player movement.", "Animation", "rts.actionreplay.animationEasing", new[] { "linear", "ease", "ease-in", "ease-out", "ease-in-out" }, "ease-in-out");
 
         AddMessages(ui);
