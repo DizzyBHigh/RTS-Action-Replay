70e8f3c6ac46501fa4920d4059d23c312bb614db
Dizzy B High <dizzybhigh@gmail.com>
2026-09-12 13:51:14 +0200

    Run end animation when replay ends

--- overlay/replay-video.js
+++ overlay/replay-video.js
@@ -154,4 +154,5 @@ RTSReplayVideo.video.addEventListener('ended', () => {
   const command = RTSReplayVideo.currentCommand;
   replayDevLog('video ended', { replayId: command?.replayId, currentTime: RTSReplayVideo.video.currentTime });
   if (command) RTSReplayVideo.notifyPlaybackEnded(command);
+  RTSReplayVideo.hideReplay();
 });
