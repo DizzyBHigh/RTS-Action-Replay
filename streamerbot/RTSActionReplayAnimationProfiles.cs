5343f88ecafc633297e7a0e046c7347fdbbe01cd
Dizzy B High <dizzybhigh@gmail.com>
2026-09-14 16:17:31 +0200

    Use resolved player positions in playback handoff

--- streamerbot/RTSActionReplayAnimationProfiles.cs
+++ streamerbot/RTSActionReplayAnimationProfiles.cs
@@ -10,7 +10,6 @@ public class CPHInline
     private const string ResolvedProfileHandoffKey = "rts.actionreplay.handoff.resolvedProfile";
     private const string PlaybackProfileHandoffKey = "rts.actionreplay.handoff.playbackProfile";
     private const string AnimationProfileHandoffKey = "rts.actionreplay.handoff.animationProfile";
-    private const string PlayerPositionsHandoffKey = "rts.actionreplay.handoff.playerPositions";
 
     public bool Execute() => EnsureProfiles();
 
@@ -118,7 +117,7 @@ public class CPHInline
         if (handoffRequested)
         {
             CPH.SetGlobalVar(AnimationProfileHandoffKey, profileJson, false);
-            CPH.SetGlobalVar(PlayerPositionsHandoffKey, playerPositions, false);
+            CPH.SetGlobalVar("rts.actionreplay.positions", playerPositions, false);
         }
         return true;
     }
