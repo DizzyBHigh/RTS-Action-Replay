03a20733307da870c440d84b6745e93f23eb15a7
Dizzy B High <dizzybhigh@gmail.com>
2026-09-18 19:13:19 +0200

    Remove obsolete search reconnect helper

--- overlay/replay-websocket.js
+++ overlay/replay-websocket.js
@@ -5,14 +5,6 @@ RTSReplayWebSocket.setStatus = (text, state = '') => {
   RTSReplayWebSocket.status.className = state;
 };
 
-RTSReplayWebSocket.resumeSearchQueue = () => {
-  RTSReplayWebSocket.socket.send(JSON.stringify({
-    request: 'DoAction',
-    id: 'rts-action-replay-search-resume',
-    action: { name: 'RTS - Action Replay - Core - Search Queue' }
-  }));
-};
-
 RTSReplayWebSocket.connect = () => {
   clearTimeout(RTSReplayWebSocket.reconnectTimer);
   const { host, port } = RTSReplayWebSocket.config;
