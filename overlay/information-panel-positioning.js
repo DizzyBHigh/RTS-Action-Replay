08d0b110d97ed806ed3f7f4e0d8b060bf5ab74f3
Dizzy B High <dizzybhigh@gmail.com>
2026-09-20 20:05:13 +0200

    Apply panel size before animation

--- overlay/information-panel-positioning.js
+++ overlay/information-panel-positioning.js
@@ -48,6 +48,8 @@ RTSInformationPanels.applyPosition = (panel, command, name) => {
 };
 
 RTSInformationPanels.show = (panel, command, name) => {
+  if (!panel) return;
+  RTSInformationPanels.applySize(panel, command);
   if (window.RTSInformationPanelAnimation) {
     RTSInformationPanelAnimation.show(panel, command, name);
     return;
