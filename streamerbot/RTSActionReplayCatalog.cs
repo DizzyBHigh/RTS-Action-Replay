fb480d41f54847c0533d105f2b929a6a35959dff
Dizzy B High <dizzybhigh@gmail.com>
2026-09-21 20:45:39 +0200

    Fix Recent chat result type

--- streamerbot/RTSActionReplayCatalog.cs
+++ streamerbot/RTSActionReplayCatalog.cs
@@ -26,7 +26,7 @@ public class CPHInline
     {
         var state = BuildState("recent", "", "recent", ParseAmount(Arg("rawInput")));
         if (CPH.GetGlobalVar<bool?>("rts.actionreplay.message.recent.chat", true) ?? true)
-            SendRecentChat(Query(state).Take(Math.Max(1, (int?)state["amount"] ?? MaxAmount())));
+            SendRecentChat(new JArray(Query(state).Take(Math.Max(1, (int?)state["amount"] ?? MaxAmount()))));
         return Queue(state);
     }
     public bool ListLastPlayed() => Queue(BuildState("lastplayed", "", "history", ParseAmount(Arg("rawInput"))));
