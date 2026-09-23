d8f4c8946e93530b3f3311ab1a8d4effd3982163
Dizzy B High <dizzybhigh@gmail.com>
2026-09-23 12:39:48 +0200

    Fix missing GeneratorStatus namespace

--- streamerbot/RTSActionReplaySettings.cs
+++ streamerbot/RTSActionReplaySettings.cs
@@ -4,6 +4,7 @@ using System.Threading;
 using System.Reflection;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Media;
 using Newtonsoft.Json.Linq;
 
