3820c23b547a1268dc959af0fc0dd52817d32632
Dizzy B High <dizzybhigh@gmail.com>
2026-09-24 18:17:12 +0200

    Remove unused replay title setting

--- streamerbot/RTSActionReplayStore.cs
+++ streamerbot/RTSActionReplayStore.cs
@@ -269,7 +269,6 @@ public class CPHInline
         AddSnapshotGlobal(globals, "httpMapping", "rts.actionreplay.httpMapping");
         AddSnapshotGlobal(globals, "httpPort", "rts.actionreplay.httpPort");
         AddSnapshotGlobal(globals, "replayTitle", "rts.actionreplay.replayTitle");
-        AddSnapshotGlobal(globals, "newReplayTitle", "rts.actionreplay.newReplayTitle");
         AddSnapshotGlobal(globals, "maxRecent", "rts.actionreplay.maxRecent");
         AddSnapshotGlobal(globals, "maxCatalog", "rts.actionreplay.maxCatalog");
         AddSnapshotGlobal(globals, "autoAdd", "rts.actionreplay.autoAdd");
